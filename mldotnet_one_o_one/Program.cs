77fe26d3c500e8f96918f57f091d3289ca459bff
nayem <nayem@SNIFFER-JR>
2023-04-16 01:46:00 +0600

    building a sentiment analysis model using binary classification

--- mldotnet_one_o_one/Program.cs
+++ mldotnet_one_o_one/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Mldotnet_one_o_one;
+
+Console.Write("Write your review here >>>>\t ");
+var input = Console.ReadLine();
+
+if (input is null) 
+    return;
+
+// Add input data
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = input.ToString(),
+};
+
+// Load model and predict output of sample data
+var result = SentimentModel.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");

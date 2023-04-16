529c8db1a024dec1f108664477c455c0e422a316
nayem <nayem@SNIFFER-JR>
2023-04-16 14:24:53 +0600

    building a taxi fare prediction model using regression algorithms

---
+++ mldotnet_one_o_two/Program.cs
@@ -0,0 +1,18 @@
+﻿using Mldotnet_one_o_two;
+
+//if (true) return 0;
+
+//Load sample data
+var sampleData = new TaxiFarePrediction.ModelInput()
+{
+    Vendor_id = @"CMT",
+    Rate_code = 1F,
+    Passenger_count = 6F,
+    Trip_distance = 1.5F,
+    Payment_type = @"CRD",
+};
+
+//Load model and predict output
+var result = TaxiFarePrediction.Predict(sampleData);
+
+Console.WriteLine($"Predicted fare amount :{result.Score}");
